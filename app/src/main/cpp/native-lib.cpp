cb4966e2e5fca340cb7340fcf173d229f33e01fd
sohamparekh92 <soham.parekh92@gmail.com>
2016-10-03 22:31:48 -0800

    Indentation

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -15,13 +15,13 @@ Java_com_example_soham_qualcommandroid_MainActivity_getTempJNI(
     std::vector<int> temp_far;
     std::vector<int> temp_cel;
     int len= env->GetArrayLength(j_temp_far);
-    //int a = env->GetIntArrayElements()
 
     jint *body = env->GetIntArrayElements(j_temp_far, 0);
 
     for(int i=0;i<len;++i){
         temp_far.push_back(body[i]);
     }
+
     temp_cel = Temperature::getInstance().getCelsius(temp_far);
     std::ostringstream resultstream;
 
@@ -30,6 +30,5 @@ Java_com_example_soham_qualcommandroid_MainActivity_getTempJNI(
     }
 
     std::string result = resultstream.str()+" - Celsius";
-
     return env->NewStringUTF(result.c_str());
 }
--- app/src/main/cpp/temperature.cpp
+++ app/src/main/cpp/temperature.cpp
@@ -10,8 +10,9 @@ Temperature& Temperature::getInstance(){
 
 std::vector<int> Temperature::getCelsius(std::vector<int> far){
     std::vector<int> result;
-    for(int i=0; i<far.size();++i)
-    result.push_back( (far[i]-32) *5/9 );
+    for(int i=0; i<far.size();++i) {
+        result.push_back((far[i] - 32) * 5 / 9);
+    }
     return result;
 }
 
