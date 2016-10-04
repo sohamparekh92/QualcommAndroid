dd069e931c3b4751a7d4107477ccdcce15ab4194
sohamparekh92 <soham.parekh92@gmail.com>
2016-10-03 17:54:49 -0800

    Double to std::string using ostringstream

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -10,10 +10,12 @@ Java_com_example_soham_qualcommandroid_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject, /* this */
         jdouble temp) {
+
     double tp = temp;
-
-    std::string hello = "Hello from C++ ";//+Temperature::getInstance(temp).getTemp();
-
+    std::ostringstream strx;
+    strx<<temp;
+    //std::string tp =
+    std::string hello = "Hello from C++ "+strx.str();//+Temperature::getInstance(temp).getTemp();
 
     return env->NewStringUTF(hello.c_str());
 }
